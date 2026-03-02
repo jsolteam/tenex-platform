6fb2a696625926ab5835770c3ea2f9d60b34f43f
Dmitry Loginov <dem.loginov@gmail.com>
2026-03-02 13:01:01 +0500

    feat: add main.go files (bot, scheduler and worker)

---
+++ cmd/scheduler/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jsol/tenex-platform/internal/platform/container"
+	"github.com/jsol/tenex-platform/internal/platform/container/components"
+	"github.com/jsol/tenex-platform/internal/platform/logger/facade"
+	"go.uber.org/zap"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+
+	cfgComp := components.NewConfig(os.Getenv("CONFIG_FILE"))
+
+	c := container.New()
+	c.Register("config", cfgComp)
+	c.Register("metrics", components.NewMetrics(cfgComp))
+	c.Register("tracing", components.NewTracing(cfgComp))
+	c.Register("logger", components.NewLogger(cfgComp))
+
+	if err := c.Start(ctx); err != nil {
+		_, _ = os.Stderr.WriteString("scheduler: container start failed: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	facade.L().Info("scheduler: started")
+
+	<-ctx.Done()
+
+	facade.L().Info("scheduler: shutting down")
+
+	shutdownTimeout := cfgComp.Get().App.ShutdownTimeout
+	shutCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := c.Stop(shutCtx); err != nil {
+		facade.L().Error("scheduler: shutdown error", zap.Error(err))
+		os.Exit(1)
+	}
+
+	facade.L().Info("scheduler: shutdown complete")
+}
