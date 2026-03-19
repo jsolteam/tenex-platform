80e6d24642ec520b7520ebb57794dfe2d0a9c126
Dmitry Loginov <dem.loginov@gmail.com>
2026-03-20 03:12:57 +0500

    ref: update tx.go errors

--- internal/infrastructure/database/tx.go
+++ internal/infrastructure/database/tx.go
@@ -3,7 +3,8 @@ package database
 import (
 	"context"
 	"database/sql"
-	"fmt"
+
+	apperrors "github.com/jsolteam/tenex-platform/internal/platform/errors"
 )
 
 type TxFunc func(ctx context.Context, tx *sql.Tx) error
@@ -11,31 +12,7 @@ type TxFunc func(ctx context.Context, tx *sql.Tx) error
 func WithTx(ctx context.Context, db *sql.DB, fn TxFunc) (retErr error) {
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
-		return fmt.Errorf("begin tx: %w", err)
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p) // re-panic after rollback
-		}
-		if retErr != nil {
-			_ = tx.Rollback()
-		}
-	}()
-
-	if err := fn(ctx, tx); err != nil {
-		return err
-	}
-	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
-	}
-	return nil
-}
-
-func WithTxOpts(ctx context.Context, db *sql.DB, opts *sql.TxOptions, fn TxFunc) (retErr error) {
-	tx, err := db.BeginTx(ctx, opts)
-	if err != nil {
-		return fmt.Errorf("begin tx: %w", err)
+		return apperrors.DBConnect("database.WithTx.begin", err)
 	}
 	defer func() {
 		if p := recover(); p != nil {
@@ -51,7 +28,31 @@ func WithTxOpts(ctx context.Context, db *sql.DB, opts *sql.TxOptions, fn TxFunc)
 		return err
 	}
 	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
+		return apperrors.DB("database.WithTx.commit", err)
+	}
+	return nil
+}
+
+func WithTxOpts(ctx context.Context, db *sql.DB, opts *sql.TxOptions, fn TxFunc) (retErr error) {
+	tx, err := db.BeginTx(ctx, opts)
+	if err != nil {
+		return apperrors.DBConnect("database.WithTxOpts.begin", err)
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		}
+		if retErr != nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	if err := fn(ctx, tx); err != nil {
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		return apperrors.DB("database.WithTxOpts.commit", err)
 	}
 	return nil
 }
